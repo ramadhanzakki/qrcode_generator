4f3ed6e60d809a39117e60c9425dc21da3f3f107
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2025-09-22 12:29:09 +0700

    added new features: user can select color of qrcode

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -2,8 +2,9 @@ import qrcode
 
 data = input('enter the text or url: ').strip()
 filename = input('enter the filename: ').strip()
+select_color = input('which the color: ').lower().strip()
 qr = qrcode.QRCode(box_size=10, border=4)
 qr.add_data(data)
-image = qr.make_image(fill_color='black', back_color='white')
+image = qr.make_image(fill_color=select_color, back_color='white')
 image.save(filename)
 print('QR code has been created')
